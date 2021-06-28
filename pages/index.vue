2a5032fa8c4ef3587c951e9c81ab1d96f47d03fd
rusirunethmina <rusiruc21@gmail.com>
2021-06-29 00:06:48 +0530

    intail

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+        <NuxtLogo />
+        <VuetifyLogo />
+      </v-card>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to the Vuetify + Nuxt.js template
+        </v-card-title>
+        <v-card-text>
+
+
+          <hr class="my-3">
+
+          <br>
+
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
